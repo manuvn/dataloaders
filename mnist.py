8a7d58234e0d0078c6ce866e7eac64346770e686
Manu V Nair <manuvn@gmail.com>
2020-09-30 22:46:32 +0200

    Add MNIST Sequential function

--- mnist.py
+++ mnist.py
@@ -7,10 +7,17 @@ from torchvision import transforms
 # Dataset preparation
 class Mnist1DTransform(object):
   """
-  Converts an MNIST image to a sequence. Ouput shape: 28*28,1
+  Converts an MNIST image to a sequence. Ouput shape: 28*28
   """
   def __call__(self, sample):
     return th.reshape(sample, [28*28])
+
+class MnistSeqTransform(object):
+  """
+  Converts an MNIST image to a sequence. Ouput shape: 28*28,1
+  """
+  def __call__(self, sample):
+    return th.reshape(sample, [28*28,1])
 
 def gen_dataloaders(data_ops, root_path = '.'):
     train_dataset = torchvision.datasets.MNIST(root=root_path, 
@@ -29,12 +36,16 @@ def gen_dataloaders(data_ops, root_path = '.'):
 
     return train_dataloader, val_dataloader
 
-def mnist_gen(flatten=False):
+def mnist_gen(flatten=False, sequential=False):
     """
-    Flatten = false will reshape images to be a 1D vector. Ex use for sequential MNIST.
+    Flatten = false will reshape images to be a 1D vector. Ex use for MLP.
     """
+    data_ops = [transforms.ToTensor()]
+
     if flatten:
         data_ops =[transforms.ToTensor(), Mnist1DTransform()]
-    else:
-        data_ops = [transforms.ToTensor()]
+
+    if sequential:
+        data_ops =[transforms.ToTensor(), MnistSeqTransform()]
+
     return gen_dataloaders(data_ops, root_path='./dataset/.')
