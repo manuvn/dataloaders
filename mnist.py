d1da9e65810d3b78fca4c39a24d14e673b70c70d
Manu V Nair <manuvn@gmail.com>
2020-09-30 22:55:52 +0200

    Add batch size options

--- mnist.py
+++ mnist.py
@@ -19,24 +19,24 @@ class MnistSeqTransform(object):
   def __call__(self, sample):
     return th.reshape(sample, [28*28,1])
 
-def gen_dataloaders(data_ops, root_path = '.'):
+def gen_dataloaders(data_ops, train_batch_size=8, val_batch_size=8, root_path = '.'):
     train_dataset = torchvision.datasets.MNIST(root=root_path, 
                                             train=True, 
                                             download=True,
                                             transform=transforms.Compose(data_ops))
 
-    train_dataloader = utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+    train_dataloader = utils.data.DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
 
     val_dataset = torchvision.datasets.MNIST(root=root_path, 
                                             train=False, 
                                             download=True,
                                             transform=transforms.Compose(data_ops))
 
-    val_dataloader = utils.data.DataLoader(val_dataset, batch_size=32, shuffle=True)
+    val_dataloader = utils.data.DataLoader(val_dataset, batch_size=val_batch_size, shuffle=True)
 
     return train_dataloader, val_dataloader
 
-def mnist_gen(flatten=False, sequential=False):
+def mnist_gen(train_batch_size=8, val_batch_size=8, flatten=False, sequential=False):
     """
     Flatten = false will reshape images to be a 1D vector. Ex use for MLP.
     """
@@ -48,4 +48,4 @@ def mnist_gen(flatten=False, sequential=False):
     if sequential:
         data_ops =[transforms.ToTensor(), MnistSeqTransform()]
 
-    return gen_dataloaders(data_ops, root_path='./dataset/.')
+    return gen_dataloaders(data_ops, train_batch_size, val_batch_size, root_path='./dataset/.')
